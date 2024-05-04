1da3a8cb85f043cf3e2553005c74f78fa3ab4727
Dan Andrei Niculae <dan.niculae@logichainge.com>
2024-05-04 14:46:42 +0300

    fuck

--- src/layouts/authentication/sign-out/index.js
+++ src/layouts/authentication/sign-out/index.js
@@ -25,26 +25,15 @@ export default function SignOut() {
   //     });
   //   };
 
-  const logoutUser = async () => {
+  useEffect(() => {
     e.preventDefault();
     axios.post(`${url}/logout/`, {}).then((response) => {
       if (response.status === 200) {
-        setCurrentUser(true);
+        setCurrentUser(false);
         navigate("/authentication/sign-in");
       }
     });
-  };
-
-  useEffect(() => {
-    const logoutAndRedirect = async () => {
-      await logoutUser();
-      setCookie("user", "", { path: "/" });
-      navigate("/authentication/sign-in");
-      console.log("User logged out");
-    };
-
-    logoutAndRedirect();
-  }, [navigate, logoutUser, setCookie]);
+  }, []);
 
   // Render nothing, as this component's sole purpose is to sign out the user
   return null;
